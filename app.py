e43db73c9059100367c337f709a94230b88cbf89
Everett Toews <everett.toews@gmail.com>
2020-02-05 10:04:25 +1300

    Add sleep route

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
 import json
+import logging
+import time
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s.%(msecs)03d, %(levelname)s, %(message)s', datefmt='%Y-%m-%dT%H:%M:%S', level=logging.DEBUG)
 
 @app.route('/')
 def hello_world():
-    hostname = {'hostname': os.environ.get('HOSTNAME', 'HOSTNAME unset')}
-    return json.dumps(hostname)
+    result = {'hostname': os.environ.get('HOSTNAME', 'HOSTNAME unset')}
+    return json.dumps(result)
+
+
+@app.route('/sleep')
+def sleep():
+    seconds = request.args.get('seconds', default=60, type=int)
+    logger.debug("sleep for %ss", seconds)
+    time.sleep(seconds)
+
+    result = {'hostname': os.environ.get('HOSTNAME', 'HOSTNAME unset'), 'seconds': seconds}
+    return json.dumps(result)
